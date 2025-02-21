594db39b1050efa7bfe6caa4d3e4529ab0afe08b
Jessica <jessicaportilio@academico.ufs.br>
2025-02-21 15:11:30 -0300

    Criação do Projeto e do Container Base

---
+++ Projeto de Cards Part1/cards-project/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  
+}
