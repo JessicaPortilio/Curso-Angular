4aec7cd33fdfe9d6014943baa0192d21c1311139
Jessica <jessicaportilio@academico.ufs.br>
2025-03-03 17:29:14 -0300

    Atributo select do ng-content

---
+++ FundamentosDoAngularProjecaoDeConteudo_e_EstilizacaoComDiretivas/AtributoSelectDoNgContent/src/app/comp-texto/comp-texto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comp-texto',
+  imports: [],
+  templateUrl: './comp-texto.component.html',
+  styleUrl: './comp-texto.component.scss'
+})
+export class CompTextoComponent {
+
+}
