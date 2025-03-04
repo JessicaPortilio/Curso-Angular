0b12976c2ddc7b1c3801dc22947fe5d4fe979236
Jessica <jessicaportilio@academico.ufs.br>
2025-03-04 08:30:24 -0300

    Diretiva com Seletor de Elemento: Modificando Textos

---
+++ FundamentosDoAngularProjecaoDeConteudo_e_EstilizacaoComDiretivas/DiretivaComSeletorDeElementoModificandoTextos/src/app/components/card/directives/card-text-subtitle.directive.ts
@@ -0,0 +1,8 @@
+import { Directive } from '@angular/core'; // Importa a funcionalidade de Diretiva do Angular
+
+@Directive({
+  selector: 'app-Card-Text-Subtitle', // Indica que essa diretiva será aplicada a elementos <app-Card-Text-Subtitle>
+  host: {'class': 'ca-u-card-text-subtitle'} // Aplica automaticamente a classe "ca-u-card-text-subtitle" a esse elemento
+})
+export class CardTextSubtitleDirective {} // Declara a classe da diretiva
+
