b9d6bc397e5547126a88355da71f78dac8625fd5
Jessica <jessicaportilio@academico.ufs.br>
2025-03-05 15:17:08 -0300

    Aplicação de Diretivas nos Card de Projeção: Header e Content

---
+++ FundamentosDoAngularProjecaoDeConteudo_e_EstilizacaoComDiretivas/AplicacaoDeDiretivasNoCardDeProjecao_HeaderEContent/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CardComponent } from "./card/card.component";
+import { CardHeaderDirective } from "./card/directives/card-header.directive";
+import { CardContentDirective } from "./card/directives/card-content.directive";
+
+@Component({
+  selector: 'app-root',
+  imports: [CardComponent, CardHeaderDirective, CardContentDirective],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'CriacaoDeUmCardComProjecaoDeConteudo';
+}
