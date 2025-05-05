be826aa24f01757b1f39518fb04f80f5b21d5736
Jessica <jessicaportilio@academico.ufs.br>
2025-05-05 19:56:15 -0300

    Sintaxe Básica dos Pipes

---
+++ FundamentosDoAngularPipes/SintaxeBasicaDosPipes/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CommonModule, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'SintaxeBasicaDosPipes';
+
+  pessoa = {
+    nome: 'Jessica',
+    idade: 26,
+  };
+}
